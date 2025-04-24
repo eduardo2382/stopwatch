a8cb3024e3945872b7b47cbe858cb3b817e7cb09
eduard2382 <motadossantosjoseeduardo.m82@gmial.com>
2025-04-24 20:34:15 -0300

    timer code working

---
+++ script.js
@@ -0,0 +1,76 @@
+const btnStart = document.querySelector("#btnStart")
+const btnPause = document.querySelector("#btnPause")
+const btnMarkTurn = document.querySelector("#btnMarkTurn")
+const spanCounting = document.querySelector('#counting')
+
+var second = 0
+var minute = 0
+var hour = 0
+
+var coutingInterval = null
+
+var turns = []
+
+btnStart.addEventListener('click', startCouting)
+
+btnMarkreturn.addEventListener('click', markTurn)
+
+function startCouting(){
+    // Inicia a contagem, adicionando e renderizando os numeros a cada 1s
+    coutingInterval = setInterval(addCouting, 1000)
+
+    // Desabilita o botao 'Iniciar' e remove o evento de click
+    btnStart.classList.toggle('disabled')
+    btnStart.removeEventListener('click', startCouting)
+
+    // Habilita o botao 'Pausar' e adiciona o evento de click
+    btnPause.classList.toggle('disabled')
+    btnPause.addEventListener('click', stopCouting)
+}
+
+function addCouting(){
+    // Função para adicionar os segundos, minutos ou horas
+
+    if(second < 59){
+        second++
+    } else if(minute < 59){
+        second = 0
+        minute++
+    } else{
+        hour++
+        minute = 0
+        second = 0
+    }
+
+    renderCouting()
+}
+
+function renderCouting(){
+    // Função para atualizar a contagem no html
+
+    spanCounting.innerHTML = formatCouting()
+}
+
+function formatCouting(){
+    // Função para retornar os numeros da contagem formatados
+
+    let hourFormatted = hour > 9 ? hour : `0${hour}`
+    let minuteFormatted = minute > 9 ? minute : `0${minute}`
+    let secondFormatted = second > 9 ? second : `0${second}`
+
+    return `${hourFormatted}:${minuteFormatted}:${secondFormatted}`
+}
+
+function stopCouting(){
+    // Para a contagem
+    clearInterval(coutingInterval)
+
+    // Habilita o botao 'Iniciar' e adiciona o evento de click
+    btnStart.classList.toggle('disabled')
+    btnStart.addEventListener('click', startCouting)
+
+    // Desabilita o botao 'Pausar' e remove o evento de click
+    btnPause.classList.toggle('disabled')
+    btnPause.removeEventListener('click', stopCouting)
+}
+
